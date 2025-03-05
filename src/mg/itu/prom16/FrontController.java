dd7eda4ea44c0815e1dcd002f05f1147b5f6949f
sergianafr <sergiravelo@gmail.com>
2025-03-05 08:31:38 +0300

    Update FrontController.java

--- src/mg/itu/prom16/FrontController.java
+++ src/mg/itu/prom16/FrontController.java
@@ -240,13 +240,6 @@ public class FrontController extends HttpServlet {
         processRequest(request, response);
     }
 
-  
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
     public String getServletInfo() {
         return "Short description";
